8a17bd6ea82700078bcd164b6cdf7517a73268f3
MegaMena <guldsmil@hotmail.com>
2018-04-13 15:07:07 +0200

    fixed javascript

--- assets/js/scriptSnd.js
+++ assets/js/scriptSnd.js
@@ -40,12 +40,13 @@ function buildList()
 
   for(var i = 0; i < commentList.length; i++)
   {
-    var liElm = document.createElement("div").classList.add("subject");
-    var textPElm = document.createElement("h3");
+    var liElm = document.createElement("div");
+    liElm.className = "subject";
+    var textPElm = document.createElement("h4");
     var delBtnElm = document.createElement("button");
     var editBtnElm = document.createElement("button");
 
-    textPElm.innerHTML = noteList[i].text;
+    textPElm.innerHTML = commentList[i].text;
 
     delBtnElm.innerHTML = "Delete";
     delBtnElm.setAttribute("data-index", i);
@@ -59,7 +60,7 @@ function buildList()
     liElm.appendChild(delBtnElm);
     liElm.appendChild(editBtnElm);
 
-    ulElm.appendChild(textElm);
+    ulElm.appendChild(textPElm);
     ulElm.appendChild(delBtnElm);
     ulElm.appendChild(editBtnElm);
   }
@@ -88,9 +89,8 @@ function submitDelEvent(event)
 }
 function submitCommentEvent(event)
 {
-  console.log("submit new comment goes here");
 
-  var commentText = document.querySelector(".commentText");
+  var commentText = document.querySelector("#commentText");
 
   submitComment(commentText.value);
   buildList();
